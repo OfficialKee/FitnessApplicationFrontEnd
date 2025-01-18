b1890429abcfa5fab4870eb33dc6b43aef1b334c
officialkee <officialkee@KEE>
2025-01-18 14:37:57 -0500

    Add project files.

---
+++ FitnessApplicationFrontEnd/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using FitnessApplicationFrontEnd.Pages;
+
+namespace FitnessApplicationFrontEnd
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new LoginPage();
+        }
+    }
+}
---
+++ FitnessApplicationFrontEnd/Pages/LoginPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace FitnessApplicationFrontEnd.Pages;
+
+public partial class LoginPage : ContentPage
+{
+	public LoginPage()
+	{
+		InitializeComponent();
+	}
+}
